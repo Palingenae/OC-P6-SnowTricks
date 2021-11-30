86a458c4aa719cadb6348f41cdb69f96ed1b916a
Olivia De Scheerder <olivia@les-tilleuls.coop>
2021-11-30 09:57:00 +0100

    feature: WIP, homepage

--- app/src/Controller/TrickController.php
+++ app/src/Controller/TrickController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends AbstractController
+class TrickController extends AbstractController
 {
     public function index(): Response
     {

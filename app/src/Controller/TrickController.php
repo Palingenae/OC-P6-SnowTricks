9aa18073f0e3a30dc09054e149143ab36a4b3206
Olivia De Scheerder <olivia@les-tilleuls.coop>
2021-12-07 15:22:18 +0100

    Feature: homepage

--- app/src/Controller/TrickController.php
+++ app/src/Controller/TrickController.php
@@ -2,13 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class TrickController extends AbstractController
 {
+    private TrickRepository $trickRepository;
+
+    public function __construct(TrickRepository $trickRepository)
+    {
+        $this->trickRepository = $trickRepository;
+    }
+    
     public function index(): Response
     {
-        return $this->render('homepage.html.twig');
+        $tricks = $this->trickRepository->findBy([],['id'=> 'DESC'], 6);
+
+        return $this->render('homepage.html.twig', [
+            'tricks' => $tricks
+        ]);
     }
 }

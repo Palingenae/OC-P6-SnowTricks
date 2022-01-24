9351eb104aed27733d53f552240c211fd301ee70
Olivia De Scheerder <olivia@les-tilleuls.coop>
2022-01-24 18:02:22 +0100

    update: tweaks

--- app/src/Form/RegistrationFormType.php
+++ app/src/Form/RegistrationFormType.php
@@ -18,6 +18,7 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('username', TextType::class, [
+                'label' => 'Nom d\'utilisateur',
                 'attr' => [
                     'class' => 'input',
                 ],
